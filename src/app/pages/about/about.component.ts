54d0456a30633ec2145911322a1cd3ed0b0f59b1
Aleksandar Vujic <aleksandar.vujic@endava.com>
2023-12-08 11:13:59 +0100

    slight modification of cards

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -4,20 +4,37 @@ import { PROFILE_PICTURE } from 'src/app/constants/resources';
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
   showMidPage: boolean = true;
   myselfImg: string = PROFILE_PICTURE;
-  skills: String[] = ['Javascript', 'Angular', 'Typescript', 'HTML', 'CSS', 'Flutter', 'Dart', 'Bootstrap 4', 'NodeJS', 'Java', 'Spring Framework', 'MySQL', 'GraphQL', 'Git', 'Jira'];
+  skills: String[] = [
+    'Javascript',
+    'Angular',
+    'Typescript',
+    'HTML',
+    'CSS',
+    'Flutter',
+    'Dart',
+    'Bootstrap 4',
+    'PrimeNG',
+    'NodeJS',
+    'Java',
+    'Spring Framework',
+    'MySQL',
+    'GraphQL',
+    'Git',
+    'Jira',
+  ];
 
   toggle() {
     this.showMidPage = !this.showMidPage;
   }
 
   downloadFile() {
-    let link = document.createElement("a");
-    link.download = "Aleksandar Vujić";
+    let link = document.createElement('a');
+    link.download = 'Aleksandar Vujić';
     link.href = 'assets/files/aleksandar_vujic.pdf';
     link.click();
   }
